bc162fff6f62835020a7803256d2bcdfd1aca4f2
gutierrezArmando <gutierrezjarr@gmail.com>
2018-10-02 14:24:12 -0700

    starting proyect

---
+++ DB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DB
+//
+//  Created by ic on 10/2/18.
+//  Copyright © 2018 UABC IPhone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
